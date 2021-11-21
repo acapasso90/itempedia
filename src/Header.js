a6c6c66334248fbbf2182bf6c3ca605b7fcbc8fe
acapasso90 <69538713+acapasso90@users.noreply.github.com>
2021-11-21 14:42:48 -0500

    Added houseware/misc/wall mounted icon and active image functionality
    Added houseware/misc/wall mounted icon and active image functionality

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import leafIcon from "./media/leaficon.png";
+import housewares from "./media/housewares.png";
+import housewaresActive from "./media/housewaresActive.png";
+import misc from "./media/misc.png";
+import activeMisc from "./media/miscActive.png";
+import wall from "./media/wall.png";
+import activeWall from "./media/wallActive.png";
 
 import { BrowserRouter as Router, Route, NavLink, Routes} from "react-router-dom";
 
 
 export default function Header(){
+    const [houseIcon, setHouseIcon] = useState(housewaresActive);
+    const [wallIcon, setWallIcon] = useState(wall);
+    const [miscIcon, setMiscIcon] = useState(misc);
+
+    const url = window.location.pathname;
+
+    function wallActive(){
+        setHouseIcon(housewares);
+        setWallIcon(activeWall);
+        setMiscIcon(misc);
+    }
+
+    function miscActive(){
+        setMiscIcon(activeMisc);
+        setHouseIcon(housewares);
+        setWallIcon(wall)
+    }
+
+    function homeActive(){
+        setMiscIcon(misc);
+        setWallIcon(wall);
+        setHouseIcon(housewaresActive);
+    }
+
+    window.addEventListener('load', function () {
+        if ( url === '/wall')
+        {wallActive()}
+// on load if url is on /deepsea runs bugLinkActive function to set the deep sea icon to "active"
+else if  (url === '/misc'){miscActive()}
+    });
+
+
     return(
 <div className="header">
 <header>
@@ -15,11 +53,22 @@ export default function Header(){
                                 <p>Itempedia</p>
                             </div>
                         </div>
-                        <div className="col">
-                            <NavLink to="/"> Housewares </NavLink>
-                            <NavLink to="/wall"> Wall Mounted </NavLink>
-                            <NavLink to="/misc"> Misc </NavLink>
-                            <NavLink to="/art"> Art </NavLink>
+                        <div className="row">
+                            <div className="iconCol">
+                                <NavLink to="/" onClick={homeActive}> Housewares </NavLink>
+                                <img src={houseIcon} alt="homewares" className="navIcon" />
+                            </div>
+                            <div className="iconCol">
+                                <NavLink to="/wall" onClick={wallActive}> Wall Mounted </NavLink>
+                                <img src={wallIcon} alt="wall-mounted" className="navIcon"  />
+                            </div>
+                            <div className="iconCol">
+                                <NavLink to="/misc" onClick={miscActive}> Misc </NavLink>
+                                <img src={miscIcon} alt="misc" className="navIcon"  />
+                            </div>
+                            <div className="iconCol">
+                                <NavLink to="/art"> Art </NavLink>
+                            </div>
                         </div>
                     </div>
                 </header>
